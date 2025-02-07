bc1cf89b894ce66b952e17531dea7309ab6ee2af
Your Name <you@example.com>
2025-02-07 16:41:35 +0530

    handle one and two character strings

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,9 +3,22 @@ require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
   describe "#add" do
+    
     it "returns 0 for an empty string" do
       calculator = StringCalculator.new
       expect(calculator.add("")).to eq(0)
     end
+
+    it "returns the number itself when only one number is passed" do
+      calculator = StringCalculator.new
+      expect(calculator.add("1")).to eq(1)
+    end
+    
+    it "returns the sum of two numbers" do
+      calculator = StringCalculator.new
+      expect(calculator.add("1,5")).to eq(6)
+    end
+
+
   end
 end
