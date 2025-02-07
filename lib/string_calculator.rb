9d9c6eb85c6433bd6a97f09f48aed263b69435be
Your Name <you@example.com>
2025-02-07 16:46:09 +0530

    Support custom delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,7 +3,14 @@ class StringCalculator
     return 0 if numbers.empty?
     return numbers.to_i if numbers.to_i.to_s == numbers
 
-    numbers.split(/,|\n/).map(&:to_i).sum 
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers.split("\n", 2)
+      delimiter = delimiter[2..]
+    else
+      delimiter = ",|\n"
+    end
+  
+    numbers.split(/#{delimiter}/).map(&:to_i).sum
     
   end
 end
